a454784a2a44658b0b2a58c67d30ccc7dec1a926
mozzzgolom <mozzzgolom@gmal.com>
2021-02-17 11:30:57 +0300

    lesson-1; added server

---
+++ script.js
@@ -0,0 +1,27 @@
+const http = require('http');
+const fs = require('fs');
+ 
+
+
+const server = http.createServer(function(req, res) { 
+    let body = null
+    try {
+        const ext = req.url.split('.')[1] //разбиваем строку по точке(/img/logo.svg) и берем второй элеимент массива
+        const isSvg = ext === 'svg'; // првоеряем что после точки у нас стоит нужное расширение
+        if (isSvg) {
+            res.setHeader('Content-Type', 'image/svg+xml'); // присваиваем нужный тип файлу
+        }
+        body = fs.readFileSync(`./public${req.url}`) 
+    } catch (err) {
+        body = fs.readFileSync('./public/index.html')
+    }
+
+    res.end(body);
+   
+});
+
+server.listen(process.env. PORT || 4000)
+
+console.log('Server started')
+
+
